8c97674bf76b4cf788f64029d38e8dce90743f44
santhosh <68135777+santhosh456s@users.noreply.github.com>
2021-03-18 19:53:50 +0530

    Greetings with Props

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+import React from "react";
+
+function Welcome({ name }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+}
+export default Welcome;
