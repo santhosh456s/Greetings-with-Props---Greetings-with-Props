041aa36d4765dd75c11ed03e624b2766c3c3190e
santhosh <68135777+santhosh456s@users.noreply.github.com>
2021-03-18 20:31:37 +0530

    iyugy

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome({ name }) {
   return (
     <div>
       <h1>Hey ! {name} </h1>
-      <h2> Welcome to Newton School. </h2>
+      <h2>Welcome to Newton School.</h2>
     </div>
   );
 }
